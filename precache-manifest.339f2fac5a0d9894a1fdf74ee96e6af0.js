22f59359343152c9c888aea04c5eb670745d7593
Jonel Nacario <jnacario@indracompany.com>
2020-10-15 22:11:06 +0800

    Updates

---
+++ precache-manifest.339f2fac5a0d9894a1fdf74ee96e6af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f141fe9fc9d41057023d0ac53e24b03f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f9c35a4cda28b4edf34",
+    "url": "/monsters-rolodex/static/css/main.1fcd463a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9c35a4cda28b4edf34",
+    "url": "/monsters-rolodex/static/js/main.8bbb8fdf.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
